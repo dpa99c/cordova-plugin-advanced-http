703c8bfd0ee2dfea737bc013f386f3fc53cec3a9
Dave Alden <dpa99c@gmail.com>
2019-06-06 14:00:00 +0100

    Replace Gradle dependency on okhttp3:okhttp-urlconnection with a local implementation adapted to work with okhttp3:okhttp:3.14+ (from which okhttp-urlconnection has been removed)

--- src/android/com/silkimen/http/OkConnectionFactory.java
+++ src/android/com/silkimen/http/OkConnectionFactory.java
@@ -1,8 +1,9 @@
 package com.silkimen.http;
 
 import okhttp3.OkHttpClient;
-import okhttp3.OkUrlFactory;
+import com.silkimen.okhttp3urlconnection.OkUrlFactory;
 
+import java.io.IOException;
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.net.URLStreamHandler;
@@ -13,14 +14,12 @@ public class OkConnectionFactory implements HttpRequest.ConnectionFactory {
 
   public HttpURLConnection create(URL url) {
     OkUrlFactory urlFactory = new OkUrlFactory(this.client);
-
     return (HttpURLConnection) urlFactory.open(url);
   }
 
   public HttpURLConnection create(URL url, Proxy proxy) {
     OkHttpClient clientWithProxy = new OkHttpClient.Builder().proxy(proxy).build();
     OkUrlFactory urlFactory = new OkUrlFactory(clientWithProxy);
-
     return (HttpURLConnection) urlFactory.open(url);
   }
 }
